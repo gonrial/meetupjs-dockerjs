d818ee430e5fa764e38f1e3a8b9d52c427d44485
Ezequiel Gonzalez Rial <gonrial@gmail.com>
2020-03-04 16:30:26 -0300

    Actualizada la lista de tips

--- src/src/utils.js
+++ src/src/utils.js
@@ -7,18 +7,19 @@ export function guid() {
 }
 
 export const defaultTodos = [
-  {id: 1, label: 'Tip #1: El orden es importante para aprovechar el cache', done:false},
-  {id: 2, label: 'Tip #2: Hacer un COPY específico para limitar la inhabilitación del cache', done:false},
-  {id: 3, label: 'Tip #3: Identificar unidades que pueden aprovechar el cache', done:false},
-  {id: 4, label: 'Tip #4: Remover dependencias innecesarias', done:false},
-  {id: 5, label: 'Tip #5: Remover el cache del administrador de paquetes', done:false},
-  {id: 6, label: 'Tip #6: Usar imágenes oficiales cuando sea posible', done:false},
-  {id: 7, label: 'Tip #7: Usar tags más específicos', done:false},
-  {id: 8, label: 'Tip #8: Probar imágenes más chicas', done:false},
-  {id: 9, label: 'Tip #9: Utilizar tini para gestionar señales', done:false},
-  {id: 10, label: 'Tip #10: Construir desde el código en un ambiente consistente', done:false},
-  {id: 11, label: 'Tip #11: Adjuntar dependencias en un paso independiente', done:false},
-  {id: 12, label: 'Tip #12: Usar multi-stage buils para remover dependencias de desarrollo', done:false},
-  {id: 13, label: 'Tip #13: Usar DOCKER_BUILDKIT=1', done:false},
-  {id: 14, label: 'Tip #14: Usar los experimental flags', done:false},
+  { id: 'a9581a2', label: 'Tip #1: Usar imágenes oficiales cuando sea posible', done: false },
+  { id: 'c2ae834', label: 'Tip #2: Usar tags más específicos', done: false },
+  { id: 'efaf857', label: 'Tip #3: Probar imágenes más chicas', done: false },
+  { id: 'ace6113', label: 'Tip #4: El orden es importante para aprovechar el cache', done: false },
+  { id: '5849dca', label: 'Tip #5: Copiar lo necesario para limitar la inhabilitación del cache', done: false },
+  { id: 'de79292', label: 'Tip #6: Identificar unidades que pueden aprovechar el cache', done: false },
+  { id: '000e6c3', label: 'Tip #7: Remover dependencias innecesarias', done: false },
+  { id: 'd063646', label: 'Tip #8: Remover el cache del administrador de paquetes', done: false },
+  { id: '8c7a495', label: 'Tip #10: Adjuntar dependencias en un paso independiente', done: false },
+  { id: '74e32c5', label: 'Tip #11: Usar multi-stage buils para remover dependencias de desarrollo', done: false },
+  { id: 'e0b9c9d', label: 'Tip #11.1: En multi-stage no es necesario usar la misma imagen base', done: false },
+  { id: '07d7fbf', label: 'Tip #12: Usar un usuario con pocos privilegios', done: false },
+  { id: '53c649e', label: 'Tip #13: Usar DOCKER_BUILDKIT=1', done: false },
+  { id: '53c649e', label: 'Tip #14: Usar experimental para tener cache', done: false },
+  { id: '90e8add', label: 'Tip #15: Usar experimental para secrets', done: false }
 ]
